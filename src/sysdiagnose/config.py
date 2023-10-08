ab38d106f69a9d4d8a0b88223fc3d85555d19d63
Robert Elwell <robert.elwell@gmail.com>
2023-10-08 17:27:55 -0400

    abspath

--- src/sysdiagnose/config.py
+++ src/sysdiagnose/config.py
@@ -1,4 +1,4 @@
-
+import os
 
 """
 This file contains all of the configuration values for the project.
@@ -8,6 +8,6 @@ This file contains all of the configuration values for the project.
 cases_file = "cases.json"
 data_folder = "/tmp/sysdiagnose_data/"
 parsed_data_folder = "/tmp/sysdiagnose_parsed_data/"
-parsers_folder = "./parsers/"
-analysers_folder = "./analyzers"
+parsers_folder = os.path.abspath("./parsers/")
+analysers_folder = os.path.abspath("./analyzers")
 debug = False
