ad3968147b01869c1ed08e62bc5b6cddf3680023
Robert Elwell <robert.elwell@gmail.com>
2023-10-08 17:29:49 -0400

    try again with paths

--- src/sysdiagnose/config.py
+++ src/sysdiagnose/config.py
@@ -8,6 +8,6 @@ This file contains all of the configuration values for the project.
 cases_file = "cases.json"
 data_folder = "/tmp/sysdiagnose_data/"
 parsed_data_folder = "/tmp/sysdiagnose_parsed_data/"
-parsers_folder = os.path.abspath("./parsers/")
-analysers_folder = os.path.abspath("./analyzers")
+parsers_folder = f"{os.path.dirname(__file__)}/parsers/"
+analysers_folder = f"{os.path.dirname(__file__)}/analyzers/"
 debug = False
